bbb0c2f3121c7bab107657c95751125f52ab3034
nguyenson257 <nguyenson25701@gmail.com>
2021-12-23 16:46:56 +0700

    fix thieu header

--- managestudents/resources/views/page/infobox.blade.php
+++ managestudents/resources/views/page/infobox.blade.php
@@ -1,4 +1,4 @@
-@extends('layoutlogin')
+@extends('layout')
 @section('content')
     <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
       <symbol id="bootstrap" viewBox="0 0 118 94">
@@ -587,3 +587,4 @@
         });
       });
     </script>
+@endsection
