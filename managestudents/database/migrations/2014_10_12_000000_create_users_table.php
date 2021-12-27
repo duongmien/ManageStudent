5f40d580e0ed2e62f10912e033740dbaff8364b4
nguyenson257 <nguyenson25701@gmail.com>
2021-12-27 23:25:14 +0700

    up user

--- managestudents/database/migrations/2014_10_12_000000_create_users_table.php
+++ managestudents/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,35 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('tbl_user', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->integer('idrole');
             $table->string('name');
             $table->string('idsv');
             $table->string('password');
+            $table->string('lop')->nullable();
+            $table->string('nganh')->nullable();
+            $table->string('khoa')->nullable();
+            $table->string('ngaysinh')->nullable();
+            $table->string('gioitinh')->nullable();
+            $table->string('tongiao')->nullable();
+            $table->string('dantoc')->nullable();
+            $table->string('cmnd')->nullable();
+            $table->string('ngaycap')->nullable();
+            $table->string('noicap')->nullable();
+            $table->string('noisinh')->nullable();
+            $table->string('email')->nullable();
+            $table->string('dienthoai')->nullable();
+            $table->string('hokhau')->nullable();
+            $table->string('ladiachicua')->nullable();
+            $table->string('tinh')->nullable();
+            $table->string('huyen')->nullable();
+            $table->string('xa')->nullable();
+            $table->string('sobaohiem')->nullable();
+            $table->string('hieuluc')->nullable();
+            $table->string('nganhang')->nullable();
+            $table->string('stk')->nullable();
+            $table->string('khuvuc')->nullable();
+            $table->string('doituong')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
@@ -29,6 +54,7 @@ class CreateUsersTable extends Migration
             
                 DB::table('tbl_user')->insert([
                     'idsv' =>'1911505310100'+$i,
+                    'idrole' =>2,
                     'name' => ($faker->randomElement($aray=array('Nguyễn','Đặng','Phan','Vũ','Trần','Lê','Phạm','Hoàng'))).' '.($faker->randomElement($aray=array('Văn','Hoàng','Thị','Anh','Trung','Thùy'))).' '.($faker->randomElement($aray=array('Anh','Vy','Ngọc','Quân','Long','Phúc','Huy','Minh','Bảo','Ngân','Khang','Như','Nhi','Hân','Thư','Linh'))),
                     'password' =>'12345',
                 ]);
